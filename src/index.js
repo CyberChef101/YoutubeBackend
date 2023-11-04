f3db78a0aa9104d0bc837d95074289559d30604e
CyberChef101 <cyberchef1001@gmail.com>
2023-11-04 22:51:26 +0530

    databse connection  added

---
+++ src/index.js
@@ -0,0 +1,14 @@
+//require('dotenv').config({path:'./env'})
+
+import dotenv from "dotenv"
+import mongoose from "mongoose";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+
+connectDB();
+
