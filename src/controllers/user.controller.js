fd5cf032a90cc2a6f5d1f21178d0e20c1d633410
CyberChef101 <cyberchef1001@gmail.com>
2023-11-23 00:14:38 +0530

    add register user conroller and Bug fixex

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,75 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {ApiError} from "./utils/ApiError.js"
+import {User} from "../models/user.model.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
+import { response } from "express";
+import {ApiResponse} from "../utils/ApiResponse.js"
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message:"OK"
+    //get data for register email & pass from frontend
+    //verify data (validation ->empty value,valid email,empty pass field)
+    //check user already exist or not  ::username and email
+    //check for images ,check for avatar
+    //upload them to cloudinary,avatar
+    //create user object -create entry in DB
+    //remove password ad refresh token feild from response
+    //check for user creation
+    //return res
+
+    const  {fullName ,email ,username ,password} = req.body
+
+    if( 
+        [fullName,email,username,password].some((field) => {//check some method in JS(advance code)
+            field?.trim() === ""
+        })
+    ) {
+        throw new ApiError (400,"All Feilds Are Required")
+    }
+    
+    const existedUser = User.findOne({
+        $or:[{ username },{ email },]
     })
+
+    if(existedUser) {
+        throw new ApiError(409,"username and email already exist")
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[o]?.path;
+
+    if(!avatarLocalPath) {
+        throw new ApiError(400,"Avatar file is required")
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+
+    if(!avatar){
+        throw new ApiError(409,"username and email already exist")
+    }
+
+    const user = await User.create( {
+        fullName,
+        avatar : avatar.url,
+        coverImage : coverImage?.url || "",
+        email,
+        password,
+        username : username.toLowercase(),
+    })
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser) {
+        throw new ApiError(500,"Something went Wrong while registering user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200,createdUser,"User Registred Successfully")
+    )
+
+
 })
 
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,7 +56,7 @@ const userSchema = new schama(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next();
 
-    this.password = bcrypt.hash(this.password,10)
+    this.password = await bcrypt.hash(this.password,10)
     next()
 })
 
@@ -64,7 +64,7 @@ userSchema.methods.isPasswordCorrect = async function (password) {
     return await bcrypt.compare(password,this.password)
 }
 
-userSchema.methods.gebe=generateAccessToken = function (){
+userSchema.methods.generateAccessToken = function (){
     return jwt.sign(
         {
             _id:this._id,
@@ -80,7 +80,7 @@ userSchema.methods.gebe=generateAccessToken = function (){
 }
 
 
-userSchema.methods.gebe=generateRefreshToken = function (){
+userSchema.methods.generateRefreshToken = function (){
     return jwt.sign(
         {
             _id:this._id,
