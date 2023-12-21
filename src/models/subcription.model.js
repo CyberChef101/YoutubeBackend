f4c44e8c881da3a358381b8f7286ff5b76cc17d7
CyberChef101 <cyberchef1001@gmail.com>
2023-12-21 12:02:23 +0530

    Add subcription model

---
+++ src/models/subcription.model.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const subciptionSchema = new Schema({
+        subscriber : {
+            type:Schema.Types.ObjectId,//one who is subribing
+            ref:"User"
+        },
+        channel:{
+            type:Schema.Types.ObjectId,//one whom 'subscriber' is subscribing
+            ref:"User"
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+
+
+export const Subcription = mongoose.model("Subcription",subciptionSchema)
